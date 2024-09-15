621f8d80472926e48ae84ab82d10551f6da43851
Vladyslav Butko <81503530+Vladislav-Butko-bvo@users.noreply.github.com>
2024-09-15 09:18:32 +0300

    Update interface.vhd

--- CRC_3_VHDL/interface.vhd
+++ CRC_3_VHDL/interface.vhd
@@ -6,8 +6,8 @@ entity CRC_init is
     generic
 	(
 		    --message lenght can be devided on user data area 
-        --in MSG_LENGTH - POLINOMIAL_LENGTH = 14 bits 
-        --and 3 bits area for control sum 
+                    --in MSG_LENGTH - POLINOMIAL_LENGTH = 14 bits 
+                    --and 3 bits area for control sum 
 		MSG_LENGTH : integer := 17;
 		    --do avaible to use 3-degree polynomial
 		POLINOMIAL_LENGTH : integer := 4
@@ -17,7 +17,7 @@ entity CRC_init is
         enable : in std_logic;
             --initiates CRC decapsulation
         enable2 : in std_logic;
-	          --message with user data and padding for control sum
+	    --message with user data and padding for control sum
         msg : in std_logic_vector(MSG_LENGTH-1 downto 0);
             --polynomial is written in binary as the coefficients.
             --It is known by both side of data channel
@@ -29,11 +29,11 @@ end CRC_init;
 
 architecture CRC_init_1 of CRC_init is
         --gets encapsulated massage from CRC_encaps 
-	      --and send it to CRC_decaps
+	--and send it to CRC_decaps
     signal w_msg_output : std_logic_vector(MSG_LENGTH-1 downto 0);  
 begin
 
-	  --CRC encapsulation before writing message at
+    --CRC encapsulation before writing message at
     --output port of the interface with CRC algorithm
 CRC_encaps : entity work.CRC	
         port map (
@@ -43,8 +43,8 @@ CRC_encaps : entity work.CRC
             msg_output=>w_msg_output
         );
         
-	 --CRC decapsulation before reading message at
-   --input port of the interface with CRC algorithm
+    --CRC decapsulation before reading message at
+    --input port of the interface with CRC algorithm
 CRC_decaps : entity work.CRC	 
         port map (
             enable=>enable2, 
